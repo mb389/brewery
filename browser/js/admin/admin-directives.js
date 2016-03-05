04128e56ae496220c23d2cb6ab251d8e922178b3
codyss <cody.schwarz@gmail.com>
2016-03-05 16:00:18 -0500

    Directive removed

--- browser/js/admin/admin-directives.js
+++
@@ -1,9 +0,0 @@
-app.directive('categoryButton', function(ReviewFactory) {
-  return {
-    restrict: 'E',
-    template: `<button type="button" class="btn btn-default btn-lg"{{>cat}}</button>`,
-    scope: {
-      cat: '=',
-    }
-  }
-})
