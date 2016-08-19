0ae7bef9ffa6a5ee0e73b6533d7bfc5d7c463735
Michael Bushoy <mbushoy@gmail.com>
2016-08-19 13:24:56 -0400

    cleanup1

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -1,15 +1,9 @@
 app.config(function ($stateProvider) {
 
-    // Register our *about* state.
-    $stateProvider.state('about', {
-        url: '/about',
-        controller: 'AboutController',
-        templateUrl: 'js/about/about.html'
+  $stateProvider.state('about', {
+    url: '/about',
+    controller: 'AboutController',
+    templateUrl: 'js/about/about.html'
     });
 
-});
-
-app.controller('AboutController', function ($scope, FullstackPics) {
-
-
-});
+  });
